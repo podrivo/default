2b6aa507f7b4e27d1120ff0b97621556de087545
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2018-01-30 02:37:59 -0200

    remove css lint 'leading-zero'

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -29,7 +29,8 @@ module.exports = function(config, log, error, success) {
           'no-color-literals': 0,
           'no-css-comments': 0,
           'force-element-nesting': 0,
-          'no-transition-all': 0
+          'no-transition-all': 0,
+          'leading-zero': 0
         }
       }))
       .pipe(sassLint.format())
